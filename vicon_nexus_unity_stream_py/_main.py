cd83ee76d921682721b3c57f66b5d9835702bdad
Ahmed Shariff <shariff.mfa@outlook.com>
2020-06-02 20:16:50 -0500

    Initial script

--- vicon_nexus_unity_stream_py/_main.py
+++ vicon_nexus_unity_stream_py/_main.py
@@ -2,8 +2,29 @@
 
 """Main script."""
 
+import log
+try:
+    from vicon_dssdk import ViconDataStream
+except ImportError:
+    log.e("Make sure vicon DataStreamSDK is not installed\n")
+    raise
+
 def main():
-    pass
+    client = ViconDataStream.Client()
+
+    log.i( 'Connecting...' )
+    while not client.IsConnected():
+        client.Connect( 'localhost:801' )
+    log.i( 'Connected to vicon data stream' )
+
+    try:
+        while client.IsConnected():
+            if client.GetFrame():
+                # send data
+                pass
+
+    except ViconDataStream.DataStreamException as e:
+        log.e( f'Error: {e}' )
 
 
 if __name__ == '__main__':  # pragma: no cover
