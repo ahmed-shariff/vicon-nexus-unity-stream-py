73ffdabf9380be01a8254989884125ee2dd9f9bf
Ahmed Shariff <shariff.mfa@outlook.com>
2020-06-08 12:13:52 -0500

    Added flask REST server and added associated CLI options

--- vicon_nexus_unity_stream_py/_main.py
+++ vicon_nexus_unity_stream_py/_main.py
@@ -3,20 +3,45 @@
 """Main script."""
 
 import log
+
+from flask import Flask
+from flask_restful import Resource, Api
+
 try:
     from vicon_dssdk import ViconDataStream
 except ImportError:
-    log.e("Make sure vicon DataStreamSDK is not installed\n")
+    log.e("Make sure vicon DataStreamSDK is installed: Follow the instructions in https://www.vicon.com/software/datastream-sdk/\n")
     raise
 
-def main():
+
+def get_client(connection=None):
+    if connection is None:
+        connection = 'localhost:801'
     client = ViconDataStream.Client()
 
-    log.i( 'Connecting...' )
+    log.i('Connecting...')
     while not client.IsConnected():
-        client.Connect( 'localhost:801' )
-    log.i( 'Connected to vicon data stream' )
+        client.Connect(connection)
+    log.i('Connected to vicon data stream')
+    return client
+    
 
+def _init_api(connection=None):
+    client = get_client(connection)
+    app = Flask("vicon-ds")
+    api = Api(app)
+
+    class ViconMarkerStream(Resource):
+        def get(self):
+            if client.IsConnected() and client.GetFrame():
+                return {"data": {"markers": 10}}
+            
+    api.add_resource(ViconMarkerStream, '/')
+    app.run()
+    
+            
+def main(connection=None):
+    client = get_client(connection)
     try:
         while client.IsConnected():
             if client.GetFrame():
--- vicon_nexus_unity_stream_py/cli.py
+++ vicon_nexus_unity_stream_py/cli.py
@@ -3,13 +3,28 @@
 import click
 import log
 
-from ._main import main
+from ._main import main, _init_api
 
-@click.command()
+@click.group()
 def _main():
     log.init()
-    main()
 
+
+@click.command()
+@click.option("-c", "--connection", default='localhost:801')
+def test(connection):
+    main(connection)
+
+
+@click.command()
+@click.option("-c", "--connection", default='localhost:801')
+def server(connection):
+    _init_api(connection)
+
+
+_main.add_command(test)
+_main.add_command(server)
+    
 
 if __name__ == '__main__':  # pragma: no cover
     _main()
