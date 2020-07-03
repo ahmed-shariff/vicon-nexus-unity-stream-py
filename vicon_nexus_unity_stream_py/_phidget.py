824c3423f673005311b66f3d3a33934f1829f485
Ahmed Shariff <shariff.mfa@outlook.com>
2020-07-02 19:08:43 -0500

    Testing phidget sensor

---
+++ vicon_nexus_unity_stream_py/_phidget.py
@@ -0,0 +1,32 @@
+from Phidget22.Phidget import *
+from Phidget22.Devices.VoltageRatioInput import *
+import time
+
+def onVoltageRatioChange(self, voltageRatio):
+    if voltageRatio > 0.00001:
+        print("VoltageRatio: " + str(voltageRatio))
+
+def main():
+    voltageRatioInput0 = VoltageRatioInput()
+    
+    voltageRatioInput0.setIsHubPortDevice(True)
+    voltageRatioInput0.setHubPort(0)
+
+    print(*[x for x in voltageRatioInput0.__dir__() if "DataInterval" in x], sep="\n")
+    
+    voltageRatioInput0.setOnVoltageRatioChangeHandler(onVoltageRatioChange)
+    
+    voltageRatioInput0.openWaitForAttachment(5000)
+
+    print(voltageRatioInput0.getMinDataInterval(), voltageRatioInput0.getMaxDataInterval())
+    voltageRatioInput0.setDataInterval(100)
+    voltageRatioInput0.setSensorValueChangeTrigger(0.01)
+    try:
+        input("Press Enter to Stop\n")
+    except (Exception, KeyboardInterrupt):
+        pass
+
+    voltageRatioInput0.close()
+
+if __name__ == "__main__":
+    main()
